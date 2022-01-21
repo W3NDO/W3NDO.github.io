c3f578292f4ecfce0ec58283879845f4b8c3f827
W3NDO <wendonynag@gmail.com>
2022-01-21 13:28:21 +0300

    first iteration

---
+++ .next/static/cKhnZU0B3eo8GcHxXeRT7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1432a4c07b836c42.css","static/chunks/pages/index-394a8f39c26b58a8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
